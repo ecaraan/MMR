0b98d8fd721fa2df8787f8f2d6e39c9067d3cf8e
ecaraan <evc31@hotmail.com>
2017-01-30 14:19:48 +0800

    Make nav item active based on current route

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,7 +1,20 @@
 import { Link } from 'react-router'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 var React = require('react');
+
+var NavLink = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.func
+    },
+    render: function () {        
+        var isActive = this.context.router.isActive(this.props.to, true);
+        var className = isActive ? 'active' : '';
+        return <li className={className}><Link {...this.props} /></li>;
+    }
+
+});
+
+module.exports = NavLink;
 
 var NavBar =  React.createClass({
     render: function() {
@@ -9,9 +22,9 @@ var NavBar =  React.createClass({
             <nav className="navbar navbar-default">
                 <div className="container-fluid">                    
                     <ul className="nav navbar-nav">
-                        <li className="active"><Link to="/">Home</Link></li>
-                        <li><Link to="/tasks">Tasks</Link></li>
-                        <li><Link to="/about">About</Link></li>                        
+                        <NavLink to="/">Home</NavLink>
+                        <NavLink to="/tasks">Tasks</NavLink>
+                        <NavLink to="/about">About</NavLink>                  
                     </ul>
                 </div>
             </nav>        
