69759e96674cdbc9c8408ccee032477036e84d54
ecaraan <evc31@hotmail.com>
2017-02-11 07:23:19 +0800

    Fixed Bug: Used props instead of state for currentPage in Pagination

--- app/components/Pagination.jsx
+++ app/components/Pagination.jsx
@@ -5,21 +5,18 @@ var Pagination = React.createClass({
         return{
             rowsPerPage: this.props.rowsPerPage,
             totalRows: this.props.totalRows,
-            currentPage: this.props.currentPage,
             lastPage: Math.ceil(this.props.totalRows / this.props.rowsPerPage)
         };
     },
     onPageChanged(page){
         this.props.onPageChanged(page);
-        this.setState({currentPage: page});
     },
     onRowsPerPageChanged(){
         let rPerPage = this.refs['customRowsPerPage'].value;
 
         this.props.onRowsPerPageChanged(rPerPage);
         this.setState({rowsPerPage: rPerPage,
-            lastPage: Math.ceil(this.props.totalRows / rPerPage),
-            currentPage: 1
+            lastPage: Math.ceil(this.props.totalRows / rPerPage)
         });
     },
     render(){
@@ -27,10 +24,10 @@ var Pagination = React.createClass({
             <div>
                 <ul className="pagination">
                     <li><a href = "#" onClick={() => {this.onPageChanged(1)}}>&laquo;</a></li>
-                    <li><a href = "#" onClick={() => {if (this.state.currentPage > 1){this.onPageChanged(this.state.currentPage - 1)}}}>&lt;</a></li>               
-                    <li><a href = "#" onClick={() => {if (this.state.currentPage < this.state.lastPage){this.onPageChanged(this.state.currentPage + 1)}}}>&gt;</a></li>
+                    <li><a href = "#" onClick={() => {if (this.props.currentPage > 1){this.onPageChanged(this.props.currentPage - 1)}}}>&lt;</a></li>               
+                    <li><a href = "#" onClick={() => {if (this.props.currentPage < this.state.lastPage){this.onPageChanged(this.props.currentPage + 1)}}}>&gt;</a></li>
                     <li><a href = "#" onClick={() => {this.onPageChanged(this.state.lastPage)}}>&raquo;</a></li>
-                    <li className="disabled"><span>Page {this.state.currentPage} of {this.state.lastPage}</span></li>              
+                    <li className="disabled"><span>Page {this.props.currentPage} of {this.state.lastPage}</span></li>              
                 </ul>        
             </div>
             <div>
